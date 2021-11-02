ff956ef88313677c8b1a65da145e9d10403880d7
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-02 16:59:08 -0400

    Populated table using *ngIf and *ngFor

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
