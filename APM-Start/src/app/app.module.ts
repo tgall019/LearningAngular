db1d5eb28d25938b79298f18a8f904ea847fe9b5
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-02 18:15:11 -0400

    Handled live input from 'Filter by' box using two-way binding (ngModel)

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
