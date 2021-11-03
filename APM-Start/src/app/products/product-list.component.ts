edd381707e805aa9a731b2c0a3afcab7ab11a80b
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-03 15:53:39 -0400

    Added a getter/setter to the search bar filtering value

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        console.log('In setter', value);
+    }
+    
     products: IProduct[] = [
         {
             "productId": 1,
@@ -40,6 +49,6 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In onInit');
+        this.listFilter = 'cart';
     }
 }
