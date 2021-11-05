9128b0860275511fbf08f9958384786e1e8b3a3b
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-05 19:55:29 -0400

    Added star rating design (not functional)

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
