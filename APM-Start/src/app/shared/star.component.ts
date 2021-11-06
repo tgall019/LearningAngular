de0b5712d9512af4ee8a8116c8eebff00ac20ad9
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-05 20:21:24 -0400

    Passed data from nested component into outter component

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -8,9 +8,13 @@ import { Component, Input, OnChanges } from "@angular/core";
 
 export class StarComponent implements OnChanges{
     @Input() rating: number = 0;
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
     cropWidth: number = 75;
 
     ngOnChanges(): void {
         this.cropWidth= this.rating * 75/5;
+    }
+    onClick(): void {
+        this.ratingClicked.emit('The rating ' + this.rating + ' was clicked!');
     }
 }
