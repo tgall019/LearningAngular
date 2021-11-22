935c1d8de5c79ee5c51f918f333565803ca149b6
Tyrone Gallardo <tgall019@fiu.edu>
2021-11-22 11:22:41 -0500

    Created the product-detail components using the CLI. Added product-details and welcome components to app module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,9 +7,12 @@ import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, 
+                StarComponent, ProductDetailComponent, WelcomeComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent]
 })
